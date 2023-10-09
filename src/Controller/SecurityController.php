33b403cbff15a43b818432eced53355c0fd5350c
Eric <e.gigondan@gmail.com>
2023-10-09 15:39:16 +0200

    create mail service for registration token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Services\MailService;
 use Doctrine\ORM\EntityManagerInterface;
 use http\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +21,7 @@ class SecurityController extends AbstractController
     {
         // redirect already logged in users to user page
          if ($this->getUser()) {
-             $this->addFlash('error', 'Vous êtes déjà connecté');
+             $this->addFlash('success', 'Vous êtes déjà connecté');
              return $this->redirectToRoute('app_default');
         }
 
@@ -40,14 +42,28 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/inscription', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager,MailService $mail): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setToken(uniqid());
+            $token = random_bytes(24);
+            $user->setToken(bin2hex($token));
+
+            $mail->to($user->getEmail());
+            $mail->subject("SnowTricks - Activation de votre compte");
+            $mail->setTemplate("mail/Activation.html.twig", [
+                'user' => $user,
+            ]);
+
+            if(!$mail->send()){
+                $this->addFlash('error', "Une erreur est survenue lors de l'envoi de l'email.Réessayer plus tard");
+                return $this->redirectToRoute('app_user_new', [
+                ], Response::HTTP_SEE_OTHER);
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
             $this->addFlash('success', 'Votre compte a bien été créé. Un email vous a été envoyé pour activer votre compte.');
@@ -61,5 +77,29 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    #[Route('/inscription/{token}', name: 'app_activation')]
+public function activation(UserRepository $userRepository, string $token, EntityManagerInterface $entityManager): Response
+    {
+        $user =$userRepository->findOneBy(['token' => $token]);
+        if(!$user || $user->getToken() === null){
+            $this->addFlash('error', 'Token inconnu ou votre compte est déjà activé.');
+            return $this->redirectToRoute('app_login', [
+            ], Response::HTTP_SEE_OTHER);
+        }
+        $user->setToken(null);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        $this->addFlash('success', 'Votre compte a bien été activé.');
+        return $this->redirectToRoute('app_login', [
+        ], Response::HTTP_SEE_OTHER);
+    }
 
+    #[Route('/mail', name: 'app_mail')]
+    public function mail(UserRepository $userRepository)
+    {
+        $user = $userRepository->findOneBy(['id' => 1]);
+        return $this->render('mail/Activation.html.twig', [
+            'user' => $user,
+        ]);
+    }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -35,12 +35,12 @@ class UserController extends AbstractController
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(User1Type::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-
+            $this->addFlash('success', "votre compte a bien été modifié.");
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -56,6 +56,8 @@ class UserController extends AbstractController
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
+            $this->addFlash('success', "L'utilisateur a bien été supprimé.");
+
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
