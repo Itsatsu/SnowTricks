641f35cf8d3155d2e761aabec5ac1e357a98f454
Eric <e.gigondan@gmail.com>
2023-10-10 10:43:58 +0200

    request view update

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
@@ -14,10 +15,15 @@ class ResetPasswordRequestFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'attr' => ['autocomplete' => 'email'],
+                'required' => true,
+                'attr' => ['autocomplete' => 'email',
+                    'placeholder' => 'mon@adresse.email'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Merci de renseigner votre adresse email',
+                    ]),
+                    new Email([
+                        'message' => 'Merci de renseigner une adresse email valide',
                     ]),
                 ],
             ])
