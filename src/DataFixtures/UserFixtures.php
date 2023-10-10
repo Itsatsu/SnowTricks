3484d3b73e1ac4eb7c34a0778656e860cae942aa
Eric <e.gigondan@gmail.com>
2023-10-10 17:42:33 +0200

    add reference for user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,19 +11,22 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setEmail('user' . $i . '@test.fr');
             $user->setPassword(password_hash('pass', PASSWORD_DEFAULT));
             $user->setToken(null);
+            $user->setUsername('username' . $i);
             $user->setFirstName('firstname' . $i);
             $user->setLastName('lastname' . $i);
             $user->setRoles(['ROLE_USER']);
             $user->setPhotoPath('/assets/images/avatar.jpg');
             $user->setPhotoName('Avatar par défaut');
             $manager->persist($user);
-            $manager->flush();
+            $this->addReference('user'. $i , $user);
         }
+        $manager->flush();
     }
-        
+
 }
