b5ec9a9d9a67f94745a1ecb145594ef20a8a4c80
Eric <e.gigondan@gmail.com>
2023-10-10 17:10:52 +0200

    add user creation fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@test.fr');
+            $user->setPassword(password_hash('pass', PASSWORD_DEFAULT));
+            $user->setToken(null);
+            $user->setFirstName('firstname' . $i);
+            $user->setLastName('lastname' . $i);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPhotoPath('/assets/images/avatar.jpg');
+            $user->setPhotoName('Avatar par défaut');
+            $manager->persist($user);
+            $manager->flush();
+        }
+    }
+        
+}
