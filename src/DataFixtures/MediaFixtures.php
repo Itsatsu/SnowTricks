b78f3fdf29617816bb42e1b0f537b35ea4b99a29
Eric <e.gigondan@gmail.com>
2023-10-17 15:35:48 +0200

    Update datafixtures

--- src/DataFixtures/MediaFixtures.php
+++ src/DataFixtures/MediaFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Media;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+
 ;
 
 class MediaFixtures extends Fixture implements DependentFixtureInterface
@@ -20,10 +21,14 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
-        for ($i = 10; $i > 0; $i--) {
+
+
+        for ($i = 13; $i > -1; $i--) {
             $media = new Media();
             $media->setName($i.'.jpg');
-            $media->setStorage('/assets/images/');
+            $trickId = $this->getReference('tricks' . $i)->getId();
+            $media->setPicture('/assets/images/triks/'.$trickId.'/media/'.$media->getName());
+            copy('public/assets/images/tricks 1.jpg', 'public/assets/images/triks/'.$trickId.'/media/'.$media->getName());
             $media->setTricks($this->getReference('tricks' . $i));
             $media->setUser($this->getReference('user' . $i));
             $media->setCreatedAt(new \DateTimeImmutable());
