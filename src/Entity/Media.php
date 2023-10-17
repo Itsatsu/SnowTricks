fad7af863f5689c479352adfd6f906bf1182fdfa
Eric <e.gigondan@gmail.com>
2023-10-17 15:34:39 +0200

    Update Media and Tricks entity

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MediaRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints AS Assert;
 
@@ -20,9 +21,6 @@ class Media
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $storage = null;
-
     #[ORM\Column]
     #[Assert\NotBlank(message: 'Veuillez renseigner si le média est une image ou une vidéo.')]
     private ?bool $isVideo = null;
@@ -34,6 +32,12 @@ class Media
     #[ORM\ManyToOne(inversedBy: 'media')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Tricks $tricks = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $video = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
 
     public function getId(): ?int
     {
@@ -60,18 +64,6 @@ class Media
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getStorage(): ?string
-    {
-        return $this->storage;
-    }
-
-    public function setStorage(string $storage): static
-    {
-        $this->storage = $storage;
 
         return $this;
     }
@@ -108,6 +100,30 @@ class Media
     public function setTricks(?Tricks $tricks): static
     {
         $this->tricks = $tricks;
+
+        return $this;
+    }
+
+    public function getVideo(): ?string
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?string $video): static
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): static
+    {
+        $this->picture = $picture;
 
         return $this;
     }
--- src/Entity/Tricks.php
+++ src/Entity/Tricks.php
@@ -7,9 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints AS Assert;
 
 #[ORM\Entity(repositoryClass: TricksRepository::class)]
+#[UniqueEntity(fields: ['name'], message: "Un tricks existe déjà avec ce nom ")]
 class Tricks
 {
     #[ORM\Id]
