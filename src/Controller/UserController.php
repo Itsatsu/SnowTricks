6614e39722ab29a48cb8e267c830890fca03dbc3
Eric <e.gigondan@gmail.com>
2023-10-25 11:28:08 +0200

    add User link in base

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\User1Type;
-use App\Repository\UserRepository;
+use App\Form\UserEditType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,13 +24,18 @@ class UserController extends AbstractController
     #[Route('/{username}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(UserType::class, $user);
+        if ($this->getUser() !== $user) {
+            $this->addFlash('danger', "Vous ne pouvez pas modifier le compte d'un autre utilisateur.");
+            return $this->redirectToRoute('app_default', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $form = $this->createForm(UserEditType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-            $this->addFlash('success', "votre compte a bien été modifié.");
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', "Votre compte a bien été modifié.");
+            return $this->redirectToRoute('app_user_show', ['username' => $user->getUsername()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
