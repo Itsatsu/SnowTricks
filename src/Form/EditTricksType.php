36a00b6b507f28c742f7d925e63fde9352dc023f
Eric <e.gigondan@gmail.com>
2023-11-09 21:18:18 +0100

    add edit type

---
+++ src/Form/EditTricksType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Media;
+use App\Entity\Tricks;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class EditTricksType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+
+                    'required' => true,
+                'attr' => [
+                    'placeholder' => 'Nom du tricks',
+                ]
+            ])
+            ->add('description', TextareaType::class,[
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Description du tricks',
+                    'rows' => '15',
+                ]
+            ])
+            ->add('pictureStorage', FileType::class, [
+                'label' => 'Image principale',
+                'mapped' => false,
+                'multiple' => false,
+                'required' => false,
+                'attr' => ['accept' => '.jpg,.jpeg', 'maxSize' => '5M','maxFiles' => '1', 'id' => 'tricks_pictureStorage', 'onchange' => "afficherApercuImage()"],
+                'constraints' => [
+                    new Assert\File([
+                        'maxSize' => '5M',
+                        'maxSizeMessage' => 'Le fichier ne doit pas dépasser 5 Mo.',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger un fichier JPG ou JPEG valide.',
+                    ])
+                ]
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Group::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
