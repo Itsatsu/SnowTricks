efa6e9cc415219e14c7074e213368d46cc10644b
Eric <e.gigondan@gmail.com>
2023-11-10 13:05:54 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-    "./node_modules/tw-elements/dist/js/**/*.js"
-  ],
-  theme: {
-    extend: {},
-    colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'background2': '#E6F3FE',
-        'primary2': '#F27C0F',
-        'secondary2': '#B9DDFD',
-        'accent': '#0679E5',
-        'texte': '#00050A',
-    },
-      fontFamily: {
-          'bree': ['Bree Serif', 'sans-serif'],
-      },
-  },
-  plugins: [
-      require('tw-elements/dist/plugin.cjs'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio')
-  ],
+	content: [
+		"./assets/**/*.js",
+		"./templates/**/*.html.twig",
+		"./node_modules/tw-elements/dist/js/**/*.js"
+	],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'background2': '#E6F3FE',
+			'primary2': '#F27C0F',
+			'secondary2': '#B9DDFD',
+			'accent': '#0679E5',
+			'texte': '#00050A',
+		},
+		fontFamily: {
+			'bree': ['Bree Serif', 'sans-serif'],
+		},
+	},
+	plugins: [
+		require('tw-elements/dist/plugin.cjs'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio')
+	],
 }
 
